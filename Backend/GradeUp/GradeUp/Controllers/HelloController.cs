4de5d1b5dcefacd25033b7c81ea0856bb4dfd97a
Sweet0220 <MNealcos@gmail.com>
2024-03-19 17:56:49 +0200

    Initial commit

---
+++ Backend/GradeUp/GradeUp/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using GradeUp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GradeUp.Controllers
+{
+    [ApiController]
+    [Route("hello")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("world")]
+        public Hello hello()
+        {
+            return new Hello("Hello World!");
+        }
+    }
+}
---
+++ Backend/GradeUp/GradeUp/Entities/Hello.cs
@@ -0,0 +1,8 @@
+﻿namespace GradeUp.Entities
+{
+    public class Hello
+    {
+        public string text { get; set; }
+        public Hello(string text) { this.text = text; }
+    }
+}
