4c7a91cc05f7947e88129680c5f127d6ea2dcd4e
Sweet0220 <MNealcos@gmail.com>
2024-03-20 00:10:25 +0200

    modified teaching_populate.sql

--- Database/populate/teaching_populate.sql
+++ Database/populate/teaching_populate.sql
@@ -1,2 +1,3 @@
 INSERT INTO teaching (id, id_user, id_subject) VALUES (1, 3, 5);
 INSERT INTO teaching (id, id_user, id_subject) VALUES (2, 4, 3);
+INSERT INTO teaching (id, id_user, id_subject) VALUES (3, 1, 10);
