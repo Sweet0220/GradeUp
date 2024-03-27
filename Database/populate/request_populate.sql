9446d20a9db1aad633af34cd0d2a2dd78fa3f2d6
DESKTOP-VHPMGK5\Maimutik <ursudenisaa@gmail.com>
2024-03-27 22:56:34 +0200

    Added request_populate.sql

---
+++ Database/populate/request_populate.sql
@@ -0,0 +1,10 @@
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (1, 2, 1, 3, true);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (2, 9, 10, 1, true);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (3, 2, 4, 2, false);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (4, 1, 2, 5, true);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (5, 6, 5, 7, false);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (6, 7, 9, 4, true);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (7, 10, 1, 3, true);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (8, 4, 5, 9, true);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (9, 4, 7, 10, true);
+INSERT INTO request(id, id_user1, id_user2, id_subject, accepted) VALUES (10, 9, 6, 6, false);
