44f8c32275375b113150fbe6083075c6d59c75bd
Sweet0220 <MNealcos@gmail.com>
2024-03-28 20:13:49 +0200

    Scripts final version

--- Database/populate/user_populate.sql
+++ Database/populate/user_populate.sql
@@ -1,11 +1,11 @@
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (1, 'popescuMonica', 'popescu_monica@student.utcluj.ro', 'hsFjb*5', 'Popescu Monica', 'F', 'user');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (2, 'adamGeorge', 'adam_george@student.utcluj.ro', 'awmG4@1', 'Adam George', 'M', 'user');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (3, 'popaStefan', 'popa_stefan@student.utcluj.ro', 'odnbA51jf', 'Popa Stefan', 'M', 'admin');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (4, 'anghelAna', 'anghel_ana@student.utcluj.ro', 'kevg43k@', 'Anghel Ana', 'F', 'user');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (5, 'casianMaria', 'casian_maria@student.utcluj.ro', 'h2jsV%hd', 'Casian Maria', 'F', 'user');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (6, 'mironDavid', 'miron_david@student.utcluj.ro', 'HbdD73$g', 'Miron David', 'M', 'user');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (7, 'diaconuDamian', 'diaconu_damian@student.utcluj.ro', '2hb%HSsfn', 'Diaconu Damian', 'M', 'user');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (8, 'predescuIrina', 'predescu_irina@student.utcluj.ro', 'HFGhj34hf', 'Predescu Irina', 'F', 'admin');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (9, 'machedonMedeea', 'machedon_medeea@student.utcluj.ro', 'Akb39fm&', 'Machedon Medeea', 'F', 'user');
-INSERT INTO user(id, username, email, password, name, sex, role) VALUES (10,'davidIoan', 'david_ioan@student.utcluj.ro', 'hnb56Rh%g', 'David Ioan','M', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (1, 'popescuMonica', 'popescu_monica@student.utcluj.ro', 'hsFjb*5', 'Popescu Monica', 'F', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (2, 'adamGeorge', 'adam_george@student.utcluj.ro', 'awmG4@1', 'Adam George', 'M', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (3, 'popaStefan', 'popa_stefan@student.utcluj.ro', 'odnbA51jf', 'Popa Stefan', 'M', 'admin');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (4, 'anghelAna', 'anghel_ana@student.utcluj.ro', 'kevg43k@', 'Anghel Ana', 'F', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (5, 'casianMaria', 'casian_maria@student.utcluj.ro', 'h2jsV%hd', 'Casian Maria', 'F', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (6, 'mironDavid', 'miron_david@student.utcluj.ro', 'HbdD73$g', 'Miron David', 'M', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (7, 'diaconuDamian', 'diaconu_damian@student.utcluj.ro', '2hb%HSsfn', 'Diaconu Damian', 'M', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (8, 'predescuIrina', 'predescu_irina@student.utcluj.ro', 'HFGhj34hf', 'Predescu Irina', 'F', 'admin');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (9, 'machedonMedeea', 'machedon_medeea@student.utcluj.ro', 'Akb39fm&', 'Machedon Medeea', 'F', 'user');
+INSERT INTO users(id, username, email, password, name, sex, role) VALUES (10,'davidIoan', 'david_ioan@student.utcluj.ro', 'hnb56Rh%g', 'David Ioan','M', 'user');
 
